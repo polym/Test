ba9c8a413d5748134fb8a917de996b98ea57a7a0
polym <352280764@qq.com>
2013-08-23 10:39:15 +0800

    Update hdu.cpp

--- hdu.cpp
+++ hdu.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int log[100005];
+void init(int n){
+    log[1] = 0;
+    for(int i = 2; i <= n; i++){
+        log[i] = 31 - __builtin_clz(i-1);
+    }
+}
 int main(){
 }
